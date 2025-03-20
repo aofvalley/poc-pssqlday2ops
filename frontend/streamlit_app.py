1e3a79e816594ccbdfa26ee59fc7da5d53d83b77
aofvalley <alfonsod@microsoft.com>
2025-03-20 11:30:27 +0100

    fix: Update .gitignore and examples for improved clarity; modify streamlit_app.py to include default API key and remove azure-keyvault-secrets from requirements

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -15,7 +15,7 @@ secrets = load_secrets()
 st.sidebar.title("PostgreSQL Backup & Restore")
 st.sidebar.title("Configuración de API")
 API_BASE_URL = st.sidebar.text_input("API Base URL", "https://alfonsodapi.azure-api.net")
-FUNCTION_KEY = st.sidebar.text_input("API Subscription Key", "", type="password")
+FUNCTION_KEY = st.sidebar.text_input("API Subscription Key", "x-PqRqUktW7SUTlvFpQhYp4IpkL7kLKfHjVchK8a04YuAzFu1NVnMA==", type="password")
 
 # Almacenar en session_state para acceso desde otras páginas
 if "api_base_url" not in st.session_state:
